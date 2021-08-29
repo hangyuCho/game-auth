b2e82a6ba1033f97926ca89a19e5434ff04201aa
root <root@li1836-131.members.linode.com>
2021-08-29 16:06:11 +0900

    add : dotenv

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb",
-  dialect: "mysql",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PW,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DB_TYPE,
   pool: {
     max: 5,
     min: 0,
